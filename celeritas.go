937a308b80db67452080faeb50a718da9aebf276
Justin Folkers <folkersj@users.noreply.github.com>
2023-08-22 16:31:31 +0200

    added render tests

--- celeritas.go
+++ celeritas.go
@@ -1,13 +1,137 @@
 package celeritas
 
-func TestFunc(a, b int) int {
-    return a + b
+import (
+    "fmt"
+    "github.com/CloudyKit/jet/v6"
+    "github.com/folkersj/celeritas/render"
+    "github.com/go-chi/chi/v5"
+    "github.com/joho/godotenv"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
+    "time"
+)
+
+const version = "1.0.0"
+
+type Celeritas struct {
+    AppName  string
+    Debug    bool
+    Version  string
+    ErrorLog *log.Logger
+    InfoLog  *log.Logger
+    RootPath string
+    Routes   *chi.Mux
+    Render   *render.Render
+    JetViews *jet.Set
+    config   config
 }
 
-func TestFunc2(a, b int) int {
-    return a - b
+type config struct {
+    port     string
+    renderer string
 }
 
-func TestFunc3(a, b int) int {
-    return a * b
+func (c *Celeritas) New(rootPath string) error {
+    pathConfig := initPaths{
+        rootPath:    rootPath,
+        folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+    }
+
+    err := c.Init(pathConfig)
+    if err != nil {
+        return err
+    }
+
+    err = c.checkDotEnv(rootPath)
+    if err != nil {
+        return err
+    }
+
+    // read .env
+    err = godotenv.Load(rootPath + "/.env")
+    if err != nil {
+        return err
+    }
+
+    // create loggers
+    infoLog, errorLog := c.startLoggers()
+    c.InfoLog = infoLog
+    c.ErrorLog = errorLog
+    c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+    c.Version = version
+    c.RootPath = rootPath
+    c.Routes = c.routes().(*chi.Mux)
+
+    c.config = config{
+        port:     os.Getenv("PORT"),
+        renderer: os.Getenv("RENDERER"),
+    }
+
+    var views = jet.NewSet(jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)), jet.InDevelopmentMode())
+    c.JetViews = views
+
+    c.createRenderer()
+
+    return nil
+}
+
+func (c *Celeritas) Init(p initPaths) error {
+    root := p.rootPath
+    for _, path := range p.folderNames {
+        //create folder if it doesn't exist
+        err := c.CreateDirIfNotExist(root + "/" + path)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
+
+// ListenAndServe starts the web server
+func (c *Celeritas) ListenAndServe() {
+    srv := &http.Server{
+        Addr:         fmt.Sprintf(":%s", os.Getenv("PORT")),
+        ErrorLog:     c.ErrorLog,
+        Handler:      c.Routes,
+        IdleTimeout:  30 * time.Second,
+        ReadTimeout:  30 * time.Second,
+        WriteTimeout: 600 * time.Second,
+    }
+
+    c.InfoLog.Printf("Listening on port %s", os.Getenv("PORT"))
+    err := srv.ListenAndServe()
+    c.ErrorLog.Fatal(err)
+}
+
+func (c *Celeritas) checkDotEnv(path string) error {
+    err := c.CreateFileIfNotExists(fmt.Sprintf("%s/.env", path))
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
+    var infoLog *log.Logger
+    var errorLog *log.Logger
+
+    infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+    return infoLog, errorLog
+}
+
+func (c *Celeritas) createRenderer() {
+    myRenderer := render.Render{
+        Renderer: c.config.renderer,
+        RootPath: c.RootPath,
+        Port:     c.config.port,
+        JetViews: c.JetViews,
+    }
+
+    c.Render = &myRenderer
 }
---
+++ render/render_test.go
@@ -0,0 +1,77 @@
+package render
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+var pageData = []struct {
+    name          string
+    renderer      string
+    template      string
+    errorExpected bool
+    errorMessage  string
+}{
+    {"go_page", "go", "home", false, "error rendering go template"},
+    {"go_page_no_template", "go", "no-file", true, "no error rendering non-existent go template while error is expected"},
+    {"jet_page", "jet", "home", false, "error rendering jet template"},
+    {"jet_page_no_template", "jet", "no-file", true, "no error rendering non-existent jet template while error is expected"},
+    {"render_engine_invalid", "foo", "home", true, "no error rendering with non-existent template engine"},
+}
+
+func TestRender_Page(t *testing.T) {
+    for _, e := range pageData {
+        r, err := http.NewRequest(http.MethodGet, "/some-url", nil)
+        if err != nil {
+            t.Error(err)
+        }
+
+        w := httptest.NewRecorder()
+
+        testRenderer.Renderer = e.renderer
+        testRenderer.RootPath = "./testdata"
+
+        err = testRenderer.Page(w, r, e.template, nil, nil)
+        if e.errorExpected {
+            if err == nil {
+                t.Errorf("%s: %s", e.name, e.errorMessage)
+            }
+        } else {
+            if err != nil {
+                t.Errorf("%s: %s: %s", e.name, e.errorMessage, err)
+            }
+        }
+    }
+}
+
+func TestRender_GoPage(t *testing.T) {
+    w := httptest.NewRecorder()
+    r, err := http.NewRequest(http.MethodGet, "/url", nil)
+    if err != nil {
+        t.Error(err)
+    }
+
+    testRenderer.Renderer = "go"
+    testRenderer.RootPath = "./testdata"
+
+    err = testRenderer.Page(w, r, "home", nil, nil)
+    if err != nil {
+        t.Error("Error rendering go page", err)
+    }
+}
+
+func TestRender_JetPage(t *testing.T) {
+    w := httptest.NewRecorder()
+    r, err := http.NewRequest(http.MethodGet, "/url", nil)
+    if err != nil {
+        t.Error(err)
+    }
+
+    testRenderer.Renderer = "jet"
+
+    err = testRenderer.Page(w, r, "home", nil, nil)
+    if err != nil {
+        t.Error("Error rendering jet page", err)
+    }
+}
